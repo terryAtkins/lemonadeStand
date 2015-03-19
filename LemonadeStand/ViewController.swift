4b32e080a786678d35091b3168eeda1c688b574b
terryAtkins <git2hubs@gmail.com>
2015-03-19 11:57:38 +0000

    create storyboard

---
+++ LemonadeStand/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  LemonadeStand
+//
+//  Created by terry atkins on 19/03/2015.
+//  Copyright (c) 2015 terry atkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // >>>>>>>> Labels <<<<<<<<<
+    @IBOutlet weak var inventoryMoneyLabel: UILabel!
+    @IBOutlet weak var inventoryLemonLabel: UILabel!
+    @IBOutlet weak var inventoryIceLabel: UILabel!
+    @IBOutlet weak var purchaseLemonCountLabel: UILabel!
+    @IBOutlet weak var purchaseIceCountLabel: UILabel!
+    @IBOutlet weak var mixLemonsAvailabilityLabel: UILabel!
+    @IBOutlet weak var mixIceAvailabilityLabel: UILabel!
+
+
+    // >>>>>>>> Buttons <<<<<<<<<
+    @IBAction func purchaseLemonsButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func purchaseLemonReduceButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func purchaseIceButton(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func purchaseIceReduceButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func mixAddLemonButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func mixSubtractLemonButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func mixAddIceButton(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func mixSubtractIceButton(sender: AnyObject) {
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
