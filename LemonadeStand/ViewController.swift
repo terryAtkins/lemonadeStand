bb37c3ff2f3ff7022bad9f83e1bb9b6b767ff92f
terryAtkins <git2hubs@gmail.com>
2015-03-19 12:55:43 +0000

    add baisc functions

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // varibles
+    
+    var money:Int = 10
+    var lemons:Int = 0
+    var iceCubes:Int = 0
+    var mixLemonCount:Int = 0
+    var mixIceCount:Int = 0
+    
     // >>>>>>>> Labels <<<<<<<<<
     @IBOutlet weak var inventoryMoneyLabel: UILabel!
     @IBOutlet weak var inventoryLemonLabel: UILabel!
@@ -22,37 +30,70 @@ class ViewController: UIViewController {
 
     // >>>>>>>> Buttons <<<<<<<<<
     @IBAction func purchaseLemonsButton(sender: AnyObject) {
+        lemons += 1
+        money -= 2
+        purchaseLemonCountLabel.text = "\(lemons)"
+        inventoryLemonLabel.text = "\(lemons) Lemon(s)"
+        inventoryMoneyLabel.text = "$\(money)"
     
     }
     
     @IBAction func purchaseLemonReduceButton(sender: AnyObject) {
-    
+        lemons -= 1
+        money += 2
+        purchaseLemonCountLabel.text = "\(lemons)"
+        inventoryLemonLabel.text = "\(lemons) Lemon(s)"
+        inventoryMoneyLabel.text = "$\(money)"
     }
     
     @IBAction func purchaseIceButton(sender: AnyObject) {
-        
+        iceCubes += 1
+        money -= 1
+        purchaseIceCountLabel.text = "\(iceCubes)"
+        inventoryIceLabel.text = "\(iceCubes) Ice cube(s)"
+        inventoryMoneyLabel.text = "$\(money)"
     }
     
     @IBAction func purchaseIceReduceButton(sender: AnyObject) {
-    
+        iceCubes -= 1
+        money += 1
+        purchaseIceCountLabel.text = "\(iceCubes)"
+        inventoryIceLabel.text = "\(iceCubes) Ice cube(s)"
+        inventoryMoneyLabel.text = "$\(money)"
     }
     
     @IBAction func mixAddLemonButton(sender: AnyObject) {
-    
+        lemons -= 1
+        mixLemonCount += 1
+        inventoryLemonLabel.text = "\(lemons) Lemon(s)"
+        mixLemonsAvailabilityLabel.text = "\(mixLemonCount)"
     }
     
     @IBAction func mixSubtractLemonButton(sender: AnyObject) {
-    
+        lemons += 1
+        mixLemonCount -= 1
+        inventoryLemonLabel.text = "\(lemons) Lemon(s)"
+        mixLemonsAvailabilityLabel.text = "\(mixLemonCount)"
+        
     }
     
     @IBAction func mixAddIceButton(sender: AnyObject) {
-    
+        iceCubes -= 1
+        mixIceCount += 1
+        inventoryIceLabel.text = "\(iceCubes) Ice cube(s)"
+        mixIceAvailabilityLabel.text = "\(mixIceCount)"
     }
     
     @IBAction func mixSubtractIceButton(sender: AnyObject) {
-    
+        iceCubes += 1
+        mixIceCount -= 1
+        inventoryIceLabel.text = "\(iceCubes) Ice cube(s)"
+        mixIceAvailabilityLabel.text = "\(mixIceCount)"
     }
     
+    @IBAction func startDayButton(sender: AnyObject) {
+    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
